f916e8fcb74ccee14944c79b32ffc4bed64f8440
Thomas Peikert <t.s.peikert@gmail.com>
2015-01-22 11:25:14 +0100

    added home as user_management landing page

--- tweap/user_management/urls.py
+++ tweap/user_management/urls.py
@@ -3,6 +3,7 @@ from user_management import views
 
 urlpatterns = patterns(
     '',
+    url(r'^$', views.Home.as_view(), name='home'),
     url(r'^register/$', views.Register.as_view(), name='register'),
     url(r'^home/$', views.Home.as_view(), name='home'),
     url(r'^login/$', views.Login.as_view(), name='login'),
