15b7f38567f6e3c0294266fd4e9a020fa2197437
Thomas Peikert <t.s.peikert@gmail.com>
2015-01-22 10:58:06 +0100

    added profile models to admin center

--- tweap/user_management/admin.py
+++ tweap/user_management/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from user_management.models import Profile
+from user_management.models import ProfileAddress
+from user_management.models import PLZ
+admin.site.register(Profile)
+admin.site.register(ProfileAddress)
+admin.site.register(PLZ)
