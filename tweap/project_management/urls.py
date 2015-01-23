e62d325cc6385ae9a217b3134a0b48b60718165e
Janneck Wullschleger <janneck_w@gmx.de>
2015-01-23 13:20:01 +0100

    added the app 'project_management', added Model project_management.Project added project_management.urls

---
+++ tweap/project_management/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, url
+from user_management import views
+
+urlpatterns = patterns(
+    '',
+    # url(r'^new/$', views.Create.as_view(), name='create'),
+
+)
--- tweap/tweap/urls.py
+++ tweap/tweap/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 urlpatterns = patterns(
     '',
     url(r'^users/', include('user_management.urls', namespace='user_management')),
+    url(r'^projects/', include('project_management.urls', namespace='project_management')),
     url(r'^admin/', include(admin.site.urls)),
 )
