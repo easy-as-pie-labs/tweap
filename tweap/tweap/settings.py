8dce8b5a3a29e19f7956801879a6e5b646b10631
Thomas Peikert <t.s.peikert@gmail.com>
2015-01-26 13:15:08 +0100

    shows profile images in profile view

--- tweap/tweap/settings.py
+++ tweap/tweap/settings.py
@@ -26,6 +26,11 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = 'http://127.0.0.1:8000/media/'
+#TODO: ja, hier muss irgendwie der lokale Pfad vom Server stehen
 
 # Application definition
 
