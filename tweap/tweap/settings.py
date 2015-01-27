5a40c3bf1f89e55cee66d81b4df60cbca9a2b132
Janneck Wullschleger <janneck_w@gmx.de>
2015-01-27 17:29:46 +0100

    bugfix missing default context_processors

--- tweap/tweap/settings.py
+++ tweap/tweap/settings.py
@@ -126,6 +126,12 @@ THUMBNAIL_PROCESSORS = (
 ) + ThumbnailSettings.THUMBNAIL_PROCESSORS
 
 TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages",
     "django.contrib.auth.context_processors.auth",
     "user_management.context_processors.user_info"
 )
