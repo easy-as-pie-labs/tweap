9fde43cf07edce26cc0d00f5b3653a65f666fc1d
Janneck Wullschleger <janneck_w@gmx.de>
2015-01-27 17:51:28 +0100

    added check if invitations is empty in project_management.tools.invite_users

--- tweap/project_management/tools.py
+++ tweap/project_management/tools.py
@@ -4,18 +4,19 @@ import re, json
 
 
 def invite_users(users, project):
-    users = json.loads(users)
-    for user in users:
-        user = user.lower().strip()
-        try:
-            if re.match("[^@]+@[^@]+\.[^@]+", user):
-                user_object = User.objects.get(email=user)
-            else:
-                user_object = User.objects.get(username=user)
+    if users:
+        users = json.loads(users)
+        for user in users:
+            user = user.lower().strip()
+            try:
+                if re.match("[^@]+@[^@]+\.[^@]+", user):
+                    user_object = User.objects.get(email=user)
+                else:
+                    user_object = User.objects.get(username=user)
 
-            if not Invitation.objects.filter(user__id=user_object.id, project__id=project.id).exists() and user_object not in project.members.all():
-                Invitation(user=user_object, project=project).save()
+                if not Invitation.objects.filter(user__id=user_object.id, project__id=project.id).exists() and user_object not in project.members.all():
+                    Invitation(user=user_object, project=project).save()
 
-        except User.DoesNotExist:
-            pass
-            # TODO: send invitation to non tweap-user via email
+            except User.DoesNotExist:
+                pass
+                # TODO: send invitation to non tweap-user via email
