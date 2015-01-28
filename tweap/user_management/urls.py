297aec1205e1d71997965584b049e6386ef942f0
goggelz <goggelz.tobi@gmail.com>
2015-01-28 13:36:04 +0100

    dashboard app created, restructured to this app

--- tweap/user_management/urls.py
+++ tweap/user_management/urls.py
@@ -3,9 +3,7 @@ from user_management import views
 
 urlpatterns = patterns(
     '',
-    url(r'^$', views.Home.as_view(), name='home'),
     url(r'^register/$', views.Register.as_view(), name='register'),
-    url(r'^home/$', views.Home.as_view(), name='home'),
     url(r'^login/$', views.Login.as_view(), name='login'),
     url(r'^logout/$', views.logout, name='logout'),
     url(r'^profile/((?P<user_name>[A-Za-z0-9]+)/)?$', views.ViewProfile.as_view(), name='profile'),
