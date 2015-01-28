902c23ec0b1b9c3d85a365a55dc83d690ef2bd3d
Janneck Wullschleger <janneck_w@gmx.de>
2015-01-28 15:19:50 +0100

    refactored create and edit in project_management.views to one view

--- tweap/project_management/urls.py
+++ tweap/project_management/urls.py
@@ -4,8 +4,8 @@ from project_management import views
 urlpatterns = patterns(
     '',
     url(r'^(?P<project_id>\d+)/?$', views.Project.as_view(), name='project'),
-    url(r'^new/$', views.Create.as_view(), name='create'),
-    url(r'^edit/(?P<project_id>\d+)/$', views.Edit.as_view(), name='edit'),
+    url(r'^new/$', views.CreateEdit.as_view(), name='create'),
+    url(r'^edit/(?P<project_id>\d+)/$', views.CreateEdit.as_view(), name='edit'),
     url(r'^all/$', views.ViewAll.as_view(), name='view_all'),
     url(r'^invites/$', views.view_invites, name='view_invites'),
     url(r'^leave_group/(?P<project_id>\d+)/?$', views.leave_group, name='leave_group'),
