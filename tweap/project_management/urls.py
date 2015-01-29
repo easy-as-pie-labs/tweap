135e96901bb7fe0b88c71491fc331446ff9a8ac2
Janneck Wullschleger <janneck_w@gmx.de>
2015-01-29 15:10:17 +0100

    added invitation_handler to urls

--- tweap/project_management/urls.py
+++ tweap/project_management/urls.py
@@ -6,10 +6,8 @@ urlpatterns = patterns(
     url(r'^(?P<project_id>\d+)/?$', views.Project.as_view(), name='project'),
     url(r'^new/$', views.CreateEdit.as_view(), name='create'),
     url(r'^edit/(?P<project_id>\d+)/$', views.CreateEdit.as_view(), name='edit'),
+    url(r'^leave/$', views.leave, name='leave'),
     url(r'^all/$', views.ViewAll.as_view(), name='view_all'),
     url(r'^invites/$', views.view_invites, name='view_invites'),
-    url(r'^leave/$', views.leave, name='leave'),
-    url(r'^accept_invite/(?P<project_id>\d+)/?$', views.accept_invite, name='accept_invite'),
-    url(r'^reject_invite/(?P<project_id>\d+)/?$', views.reject_invite, name='reject_invite'),
-
+    url(r'^invitation_handler/$', views.invitation_handler, name='invitation_handler'),
 )
