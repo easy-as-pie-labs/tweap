ae33dc44e2992913e02398c01456cf634093c733
Thomas Peikert <t.s.peikert@gmail.com>
2015-01-31 13:35:53 +0100

    added tabindexes to register, login, create and edit project

--- tweap/project_management/templatetags/myfilters.py
+++ tweap/project_management/templatetags/myfilters.py
@@ -2,6 +2,14 @@ from django import template
 
 register = template.Library()
 
+@register.filter(name='addcssandtabindex')
+def addcssandtabindex(value, arg):
+    return value.as_widget(attrs={'class': arg, 'tabindex': '1'})
+
 @register.filter(name='addcss')
 def addcss(value, arg):
     return value.as_widget(attrs={'class': arg})
+
+@register.filter(name='addtabindex')
+def addtabindex(value, arg):
+    return value.as_widget(attrs={'tabindex': arg})
