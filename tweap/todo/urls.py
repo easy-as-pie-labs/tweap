102fde6720787580cbe218a5b40f211cf6929139
Jonas Möller <jonas.moeller@web.de>
2015-02-04 16:39:42 +0100

    fixed ajax-request for tagsuggestions

--- tweap/todo/urls.py
+++ tweap/todo/urls.py
@@ -7,5 +7,4 @@ urlpatterns = patterns(
     url(r'^(?P<todo_id>\d+)/?$', views.CreateEdit.as_view(), name='todo'),
     url(r'^new/project/(?P<project_id>\d+)$', views.CreateEdit.as_view(), name='create'),
     url(r'^edit/(?P<todo_id>\d+)/$', views.CreateEdit.as_view(), name='edit'),
-    url(r'^tag_suggestion/$', projectviews.TagSuggestion.as_view(), name='tag_suggestion'),
 )
