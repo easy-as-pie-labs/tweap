a40f921c1e10b0ba1705443e08c91f37dea8cd9a
Janneck Wullschleger <janneck_w@gmx.de>
2015-02-11 15:50:57 +0100

    styled tags in edit/create todo refactored javascript for tag-management tag-handling in view styling

--- tweap/tweap/templates/edit_todo_form_control.js
+++ tweap/tweap/templates/edit_todo_form_control.js
@@ -1,8 +1,7 @@
 {% load i18n %}
 
-//Adds new Inputfields
+//Listener for add tag button
 $(document).on('click', '.addTagButton', function() {
-
     // if input is empty, prevent user adding
     if(!$(this).prev().val()) {
         // focus on empty field and give error message
@@ -11,57 +10,43 @@ $(document).on('click', '.addTagButton', function() {
         $(this).parent().parent().addClass('has-error');
     }
     else {
+        $(this).parent().parent().removeClass('has-error');
         addTagInput();
     }
 });
 
-// if input is there add new field, make old field uneditable and removable
+// adds tag from input to tag-list, cleans tag input field, refreshes tag-array
 function addTagInput(){
-    $('.addTagButton').addClass('removeTagButton');
-    $('.addTagButton').removeClass('addTagButton');
-
-    $('.removeTagButton').children().first().removeClass('glyphicon-plus-sign');
-    $('.removeTagButton').children().first().addClass('glyphicon-minus-sign');
-
-    $('.removeTagButton').prev().attr("disabled", true);
-
-    $('.removeTagButton').parent().parent().removeClass('has-error');
-
-    $('#newInputs').prepend(
-        "<div class='form-group'><div class='input-group symbol'><input id='tags' type='text' placeholder='{% trans "Add Tag" %}' class='form-control tag'><span class='input-group-addon addTagButton focus-pointer'><i class='glyphicon glyphicon-plus-sign'></i></span></div></div>"
-    );
-
-    $('#suggestions').remove();
-    $('#newInputs div:first-child').after("<div id='suggestions'></div>");
-
-    $('.removeTagButton').click(function(){
-        $(this).parent().parent().remove();
-    })
+    var newTagName = $('#tag-input').val();
+    tagList.add(newTagName);
+    $('#tag-list').append('<span class="tweap-tag label label-primary" id="' + newTagName + '"><i class="fa fa-tag"></i> ' + newTagName + '</span>');
+    $('#tag-input').val("");
+    $('#tag-input').attr("placeholder", "{% trans 'Add Tag' %}");
 }
 
-//Ajax-Request for Usersuggestions
-$(document).on('keyup', '#tags', function(){
+//adds clicked suggestion to tag-list
+$(document).on('click', '.suggestion', function() {
+    var newTagName = $(this).attr('id');
+    tagList.add(newTagName);
+    $('#tag-list').append('<span class="tweap-tag label label-primary" id="' + newTagName + '"><i class="fa fa-tag"></i> ' + newTagName + '</span>');
+    $('#suggestions').empty();
+    $('#tag-input').val("");
+    $('#tag-input').attr("placeholder", "{% trans 'Add Tag' %}");
+});
+
+//Ajax-Request for TagSuggestions
+$(document).on('keyup', '#tag-input', function(){
     typedText = (this).value
     //AJAX-Request:
     data = {search:typedText, project_id:"{{ project.id }}"};
     $.get("{% url 'project_management:tag_suggestion' %}", data, function(output){
         manageTagSuggestionAjaxRequest(output)
     })
-
-});
-
-//Puts the ID of an Suggestionelement into the value of the first inputelement in #newInputs and adds a new inputField
-$(document).on('click', '.suggestion', function() {
-    suggestionId = $(this).attr('id');
-    firstInput = $('#newInputs').find('input[type=text]').filter(':visible:first')
-    firstInput.val(suggestionId);
-    addTagInput();
 });
 
 /*
-is called on an AjaxRequest - deletes all #suggestion-HTMLElements
-and calls addSuggestionsToContent. Adding of the HTMLElements is done for each
-User inside the data-Array.
+is called on an AjaxRequest - Response
+deletes all suggestions and adds the suggestions from the response
  */
 function manageTagSuggestionAjaxRequest(data){
     $('#suggestions').empty();
@@ -71,42 +56,46 @@ function manageTagSuggestionAjaxRequest(data){
 }
 
 /*
-adds #suggestions-HTMLElements over the inputs.
+adds #suggestions-HTMLElements under the inputs.
  */
-function addSuggestionToContent(id) {
+function addSuggestionToContent(newTagName) {
     $('#suggestions').append(
-        '<h3 class="suggestion" id="' + id + '"><span class="label label-info focus-pointer">' + id + '</span></h3>'
+        '<h3 class="suggestion" id="' + newTagName + '"><span class="label label-info focus-pointer">' + newTagName + '</span></h3>'
     );
 }
 
-//Adds Userarray-JSONString to Hiddenfield
-function insertHiddenValues(){
-    inputTagArr = $("#newInputs .tag");
+//removes tags when clicked from tag-list and Array
+$(document).on('click', '.tweap-tag', function() {
+    var tagToRemove = $(this).attr('id');
+    tagList.remove(tagToRemove);
+    $(this).remove();
+});
 
-    for(i=0;i<inputTagArr.length;i++){
-        newTags.addTag(inputTagArr.eq(i).val());
-    }
-
-    $('#hiddenValues').val(newTags.getTagsString());
-}
 
 //Object is initialized on the very Beginning of this document
 var Tags = function(){
     this.Tags = new Array();
 
     //Adds new Userstring to Users-Array (Attribute)
-    this.addTag = function(added_user){
-        this.Tags.push(added_user)
+    this.add = function(tagToAdd){
+        this.Tags.push(tagToAdd);
+        $('#tags').val(JSON.stringify(this.Tags));
     }
 
-    //Stringifies the Users-Arrayattribute and returns it
-    this.getTagsString = function() {
-        return JSON.stringify(this.Tags);
+    this.remove = function(tagToRemove){
+        this.Tags.pop(tagToRemove);
+        $('#tags').val(JSON.stringify(this.Tags));
     }
 }
 
 $(document).ready(function(){
-   newTags = new Tags();
+    tagList = new Tags();
+
+    //add existing tags to tagList
+    $('.tweap-tag').each(function() {
+       tagList.add($(this).attr('id'));
+    });
+
     $('#due_date_warning').hide();
 
     $('#due_date').change(function() {
