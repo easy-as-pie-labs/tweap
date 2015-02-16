401b16d4f709c52bcc1db0611a11339ed98659b2
goggelz <goggelz.tobi@gmail.com>
2015-02-16 15:36:03 +0100

    refactored casperjs tests for deployment, FAILING PROJECT_MANAGEMENT TESTS

--- tweap/user_management/tests/test-ui.js
+++ tweap/user_management/tests/test-ui.js
@@ -1,5 +1,5 @@
 //var baseUrl = "http://127.0.0.1:8000";
-var baseUrl = "http://alpha.tweap-easy-as-pie.de";
+var baseUrl = "http://alpha.tweap.easy-as-pie.de";
 
 casper.test.begin('register account', function suite(test) {
     casper.start(baseUrl + "/users/register", function () {
@@ -7,16 +7,21 @@ casper.test.begin('register account', function suite(test) {
         test.assertUrlMatch("/register/", "register page was found");
     });
 
-    casper.then(function () {
+    casper.waitForSelector('#register-form', function () {
         this.fillSelectors('form#register-form', {
             'input[name = username]': 'casperjs',
             'input[name = email]': 'casperjs@gmail.com',
             'input[name = password]': 'test'
         });
-        this.click('.btn');
     });
 
     casper.then(function () {
+        this.evaluate(function () {
+            $('form#register-form').submit();
+        });
+    });
+
+    casper.waitForUrl(/dashboard\/$/, function () {
         test.assertUrlMatch("/dashboard/", "register and redirect to dashboard successful");
         casper.waitForSelector("#navbar_logout_link").thenClick("#navbar_logout_link");
     });
@@ -36,20 +41,25 @@ casper.test.begin('login', function suite(test) {
         test.assertUrlMatch("/login/", "login page was found");
     });
 
-    casper.then(function () {
+    casper.waitForSelector('#login-form', function () {
         this.fillSelectors('form#login-form', {
             'input[name = username]': 'casperjs',
             'input[name = password]': 'test'
         });
-        this.click('.btn');
     });
 
     casper.then(function () {
+        this.evaluate(function () {
+            $('form#login-form').submit();
+        });
+    });
+
+    casper.waitForUrl(/dashboard\/$/, function () {
         test.assertUrlMatch("/dashboard/", "login and redirect to dashboard successful");
         casper.waitForSelector("#navbar_logout_link").thenClick("#navbar_logout_link");
     });
 
-    casper.then(function () {
+    casper.waitForSelector('#navbar_login_link', function () {
         test.assertExists('#navbar_login_link', "logout successful");
     });
 
@@ -64,36 +74,41 @@ casper.test.begin('delete account', function suite(test) {
         test.assertUrlMatch("/login/", "login page was found");
     });
 
-    casper.then(function () {
+    casper.waitForSelector('#login-form', function () {
         this.fillSelectors('form#login-form', {
             'input[name = username]': 'casperjs',
             'input[name = password]': 'test'
         });
-        this.click('.btn');
     });
 
     casper.then(function () {
+        this.evaluate(function () {
+            $('form#login-form').submit();
+        });
+    });
+
+    casper.waitForUrl(/dashboard\/$/, function () {
         test.assertUrlMatch("/dashboard/", "login and redirect to dashboard successful");
         casper.waitForSelector("#navbar_profile_link").thenClick("#navbar_profile_link");
     });
 
-    casper.then(function () {
+    casper.waitForUrl(/profile\/casperjs\/$/, function () {
         test.assertUrlMatch("/profile/casperjs/", "profile page was found");
         casper.waitForSelector("#make_changes").thenClick("#make_changes");
     });
 
-    casper.then(function () {
+    casper.waitForUrl(/editprofile/, function () {
         test.assertUrlMatch("/editprofile/", "edit profile page was found");
         casper.waitForSelector("#delete_account").thenClick("#delete_account");
     });
 
-    casper.then(function () {
+    casper.waitForUrl(/delete\/$/, function () {
         test.assertUrlMatch("/delete/", "delete profile page was found");
         casper.waitForSelector("#confirm").thenClick("#confirm");
         casper.waitForSelector("#delete_account").thenClick("#delete_account");
     });
 
-    casper.then(function () {
+    casper.waitForUrl(/dashboard\/$/, function () {
         test.assertUrlMatch("/dashboard/", "delete account and redirect to dashboard successful");
     });
 
@@ -108,35 +123,48 @@ casper.test.begin('edit profile', function suite(test) {
         test.assertUrlMatch("/register/", "register page was found");
     });
 
-    casper.then(function () {
+    casper.waitForSelector('#register-form', function () {
         this.fillSelectors('form#register-form', {
             'input[name = username]': 'casperjs',
             'input[name = email]': 'casperjs@gmail.com',
             'input[name = password]': 'test'
         });
-        this.click('.btn');
     });
 
     casper.then(function () {
+        this.evaluate(function () {
+            $('form#register-form').submit();
+        });
+    });
+
+    casper.waitForUrl(/dashboard\/$/, function () {
         test.assertUrlMatch("/dashboard/", "register and redirect to dashboard successful");
         casper.waitForSelector("#navbar_profile_link").thenClick("#navbar_profile_link");
     });
 
-    casper.then(function () {
+    casper.waitForUrl(/profile\/casperjs\/$/, function () {
         test.assertUrlMatch("/profile/casperjs/", "profile page was found");
         casper.waitForSelector("#make_changes").thenClick("#make_changes");
     });
 
-    casper.then(function () {
+    casper.waitForUrl(/editprofile/, function () {
         test.assertUrlMatch("/editprofile/", "edit profile page was found");
-        this.fillSelectors('form#edit_profile_form', {
-            'input[name = first_name]': 'Casper',
-            'input[name = last_name]': 'JS'
+
+        casper.waitForSelector('#edit_profile_form', function () {
+            this.fillSelectors('form#edit_profile_form', {
+                'input[name = first_name]': 'Casper',
+                'input[name = last_name]': 'JS'
+            });
         });
-        this.click('#save_changes');
+
+        casper.then(function () {
+            this.evaluate(function () {
+                $('form#edit_profile_form').submit();
+            });
+        });
     });
 
-    casper.then(function () {
+    casper.waitForUrl(/profile\/casperjs\/$/, function () {
         test.assertUrlMatch("/profile/casperjs/", "profile was updated and redirected to profile view");
         test.assertEvalEquals(function () {
             return __utils__.findOne('#profile_name').textContent;
@@ -147,16 +175,24 @@ casper.test.begin('edit profile', function suite(test) {
         casper.waitForSelector("#make_changes").thenClick("#make_changes");
     });
 
-    casper.then(function () {
+    casper.waitForUrl(/editprofile/, function () {
         test.assertUrlMatch("/editprofile/", "edit profile page was found");
-        this.fillSelectors('form#edit_profile_form', {
-            'input[name = first_name]': 'Casper',
-            'input[name = last_name]': ''
+
+        casper.waitForSelector('#edit_profile_form', function(){
+            this.fillSelectors('form#edit_profile_form', {
+                'input[name = first_name]': 'Casper',
+                'input[name = last_name]': ''
+            });
         });
-        this.click('#save_changes');
+
+        casper.then(function () {
+            this.evaluate(function () {
+                $('form#edit_profile_form').submit();
+            });
+        });
     });
 
-    casper.then(function () {
+    casper.waitForUrl(/profile\/casperjs\/$/, function () {
         test.assertUrlMatch("/profile/casperjs/", "profile was updated and redirected to profile view");
         test.assertEvalEquals(function () {
             return __utils__.findOne('#profile_name').textContent;
@@ -167,16 +203,23 @@ casper.test.begin('edit profile', function suite(test) {
         casper.waitForSelector("#make_changes").thenClick("#make_changes");
     });
 
-    casper.then(function () {
+    casper.waitForUrl(/editprofile/, function () {
         test.assertUrlMatch("/editprofile/", "edit profile page was found");
-        this.fillSelectors('form#edit_profile_form', {
-            'input[name = first_name]': '',
-            'input[name = last_name]': 'JS'
+        casper.waitForSelector('#edit_profile_form', function () {
+            this.fillSelectors('form#edit_profile_form', {
+                'input[name = first_name]': '',
+                'input[name = last_name]': 'JS'
+            });
         });
-        this.click('#save_changes');
+
+        casper.then(function () {
+            this.evaluate(function () {
+                $('form#edit_profile_form').submit();
+            });
+        });
     });
 
-    casper.then(function () {
+    casper.waitForUrl(/profile\/casperjs\/$/, function () {
         test.assertUrlMatch("/profile/casperjs/", "profile was updated and redirected to profile view");
         test.assertEvalEquals(function () {
             return __utils__.findOne('#profile_name').textContent;
@@ -188,18 +231,18 @@ casper.test.begin('edit profile', function suite(test) {
         casper.waitForSelector("#make_changes").thenClick("#make_changes");
     });
 
-    casper.then(function () {
+    casper.waitForUrl(/editprofile/, function () {
         test.assertUrlMatch("/editprofile/", "edit profile page was found");
         casper.waitForSelector("#delete_account").thenClick("#delete_account");
     });
 
-    casper.then(function () {
+    casper.waitForUrl(/delete\/$/, function () {
         test.assertUrlMatch("/delete/", "delete profile page was found");
         casper.waitForSelector("#confirm").thenClick("#confirm");
         casper.waitForSelector("#delete_account").thenClick("#delete_account");
     });
 
-    casper.then(function () {
+    casper.waitForUrl(/dashboard\/$/, function () {
         test.assertUrlMatch("/dashboard/", "delete account and redirect to dashboard successful");
     });
 
