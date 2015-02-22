b019784b0097a6d3086186c45af60832aa8f95d0
Thomas Peikert <t.s.peikert@gmail.com>
2015-02-22 11:29:27 +0100

    added click function to calendar to create a new event

--- tweap/tweap/templates/calendar_control.js
+++ tweap/tweap/templates/calendar_control.js
@@ -10,12 +10,14 @@ $(document).ready(function() {
         editable: true,
         eventLimit: true, // allow "more" link when too many events
         eventDrop: function(event, delta, revertFunc) {
-
             updateCalendarEntry(event, revertFunc);
         },
         eventResize: function(event, delta, revertFunc) {
-
             updateCalendarEntry(event, revertFunc);
+        },
+        dayClick: function(date, jsEvent, view) {
+            console.log('Clicked on: ' + date.format());
+            // TODO: use date data to prefill a new event modal
         },
         events: [
             {% for event in events %}
@@ -31,7 +33,7 @@ $(document).ready(function() {
     });
 
     var updateCalendarEntry = function(event, revertFunc){
-        var start= event.start.format().replace('T', ' ');;
+        var start= event.start.format().replace('T', ' ');
         var end = event.end.format().replace('T', ' ');
 
         var data = {
