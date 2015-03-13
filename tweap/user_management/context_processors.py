e8d8a31fb646147ba0f861bb39661f2040a802da
Thomas Peikert <t.s.peikert@gmail.com>
2015-03-13 17:01:13 +0100

    projects dropdown now sorted alphabetically (#188)

--- tweap/user_management/context_processors.py
+++ tweap/user_management/context_processors.py
@@ -4,7 +4,7 @@ from project_management.models import Project
 def user_info(request):
     projects = None
     if request.user.is_authenticated():
-        projects = Project.objects.filter(members=request.user)
+        projects = Project.objects.filter(members=request.user).order_by('name')
 
     return {
         'username': request.user.username,
