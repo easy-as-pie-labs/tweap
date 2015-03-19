6288d855e16955e7daf9580203eb0bb848a26a7a
Janneck Wullschleger <janneck_w@gmx.de>
2015-03-19 17:53:09 +0100

    added first reauth/auth logic to chat

--- nodejs/tweap-chat.js
+++ nodejs/tweap-chat.js
@@ -7,22 +7,60 @@ console.log("tweap-chat running on port 3000...");
 
 io.on('connection', function(socket) {
 
-  // receiving login
-  socket.on('login', function(user) {
+  var authenticated = false;
+
+  console.log("new client connected");
+
+  setTimeout(function() {
+    if (!authenticated) {
+      console.log("no authentication in 1000ms -> disconnect");
+      socket.client.disconnect();
+    }
+  }, 1000);
+
+  socket.on('auth', function(request) {
     //authenticate here
-    var auth = true;
-    if (auth) {
-      clientManager.addClient(new User(user, this));
+    if (!clientManager.getBySocket(this)) {
+      var authen = true;
+      if (authen) {
+        clientManager.add(new Client(request.username, this));
+        console.log("correct authentication!");
+        authenticated = true;
+      } else {
+        console.log("wrong authentication -> disconnect");
+        this.client.disconnect();
+      }
     } else {
+      console.log("double auth -> no action");
+    }
+
+  });
+
+  socket.on('reauth', function(request) {
+    var client = clientManager.getByName(request.username);
+    if (client && !client.connected && request.reAuthToken == client.reAuthToken) {
+          client.connected = true;
+          client.socket = this;
+          console.log("correct reauth");
+          authenticated = true;
+    } else {
+      console.log("wrong reauth -> disconnect");
       this.client.disconnect();
     }
   });
 
+
+
   // receiving a message
   socket.on('message', function(msg) {
-    io.to(msg.conversation).emit('message', msg);
+    if (auth) {
+      io.to(msg.conversation).emit('message', msg);
     // add message via tweap to db
     // tweap.addMessage(msg);
+    console.log("message sended");
+  } else {
+    console.log("not authenticated -> no message");
+  }
   });
 
   // receiving request to start new conversation
@@ -30,10 +68,10 @@ io.on('connection', function(socket) {
     // var conv = tweap.getOrAddConversation(conv);
     // dev placeholder for testing
     var conv = {'id': 157, 'users': ['jawu', 'thomas']};
-    for (var user of conv.users) {
-      var sock = clientManager.getSocketOfUser(user);
-      if (sock) {
-        clientManager.getSocketOfUser(user).join(conv.id);
+    for (var username of conv.users) {
+      var client = clientManager.getByName(username);
+      if (client) {
+        client.socket.join(conv.id);
         console.log(user + " is connected and added");
       } else {
         console.log(user + " is not connected");
@@ -44,6 +82,15 @@ io.on('connection', function(socket) {
 
   // receiving disconnect
   socket.on('disconnect', function() {
+    var client = clientManager.getBySocket(this);
+    if (client) {
+      client.connected = false;
+      setTimeout(function() {
+          //here must socket be the parameter
+          clientManager.removeBySocket(this);
+      }, 10000);
+    }
+
     console.log('disconnected: ' + this.id);
   });
 
@@ -56,31 +103,64 @@ io.on('connection', function(socket) {
 
 var clientManager = new ClientManager();
 
+setInterval(function() {
+  clientManager.showClients();
+}, 1000);
+
 function ClientManager() {
   var clients = [];
 
-  this.addClient = function(user) {
-    clients.push(user);
+  this.add = function(client) {
+    clients.push(client);
     //add client to conversation rooms
-    for (var conversation of getConversationsOfUser(user.name)) {
-      user.socket.join(conversation);
+    for (var conversation of getConversationsOfUser(client.name)) {
+      client.socket.join(conversation);
+    }
+  };
+
+  this.removeBySocket = function(socket) {
+    console.log("called remove with " + socket.id);
+    for (var i = 0; i < clients.length; i++) {
+      if (clients[i].socket == socket) {
+        clients.splice(i, 1);
+        console.log("removed because of no reauth");
+        return;
+      }
     }
   }
 
-  this.getSocketOfUser = function(username) {
+  this.getByName = function(username) {
     for (var client of clients) {
-      if (client.name == username) {
-        return client.socket;
+      if (client.username == username) {
+        return client;
+      }
+    }
+    return false;
+  };
+
+  this.getBySocket = function(socket) {
+    for (var client of clients) {
+      if (client.socket == socket) {
+        return client;
       }
     }
     return false;
   }
+
+  this.showClients = function() {
+    console.log("Client-List.....");
+    for (var client of clients) {
+        console.log(client.username + " - " + client.socket.id + " - " + client.connected);
+    }
+  }
 }
 
 
-function User(name, socket) {
-  this.name = name;
+function Client(name, socket) {
+  this.username = name;
   this.socket = socket;
+  this.connected = true;
+  this.reAuthToken = "ra";
 }
 
 var getConversationsOfUser = function(user) {
