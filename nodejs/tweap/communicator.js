cba0cd7b3e0a8b7eb6350e0002595d7b527680e4
Janneck Wullschleger <janneck_w@gmx.de>
2015-03-20 11:33:45 +0100

    refactored chat nodejs files, added modules to gitignore

---
+++ nodejs/tweap/communicator.js
@@ -0,0 +1,14 @@
+if (typeof define !== 'function') {
+    var define = require('amdefine')(module);
+}
+
+define(function() {
+
+    var Client = require('./client.js');
+
+    function Communicator(socket, clientManager) {
+
+    }
+
+    return Communicator;
+});
