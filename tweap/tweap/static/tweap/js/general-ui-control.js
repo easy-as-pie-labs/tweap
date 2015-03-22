82695973598c258292cc66ecb882020f86cd5ce2
Thomas Peikert <t.s.peikert@gmail.com>
2015-03-22 08:51:24 +0100

    fixes ui bug

--- tweap/tweap/static/tweap/js/general-ui-control.js
+++ tweap/tweap/static/tweap/js/general-ui-control.js
@@ -1,35 +1,55 @@
 // toggle box and indicator icons
 $(document).on('click', '.toggle_header', function() {
-    $(this).next('.toggle_content').slideToggle();
+    if($(this).parent().attr('id') == "chat-panel")
+        chatToggle($(this));
+    else {
+        $(this).next('.toggle_content').slideToggle();
 
-    var iconHolderFirst = $(this).children().first();
-    var iconHolderLast = $(this).children().last();
+        var iconHolderFirst = $(this).children().first();
+        var iconHolderLast = $(this).children().last();
 
-    if(iconHolderFirst.hasClass('fa-chevron-right') || iconHolderFirst.hasClass('fa-chevron-down')) {
-        toggleIcon(iconHolderFirst, $(this));
-    } else {
-        toggleIcon(iconHolderLast, $(this));
+        if(iconHolderFirst.hasClass('fa-chevron-right') || iconHolderFirst.hasClass('fa-chevron-down')) {
+            toggleIcon(iconHolderFirst);
+        } else {
+            toggleIcon(iconHolderLast);
+        }
     }
 
 });
 
 /**
  * changes toggle indicator icon (open / closed) for given jquery element
- * @param iconHolder
+ * @param iconHolder element with
  */
-function toggleIcon(iconHolder, element){
+function toggleIcon(iconHolder) {
     if(iconHolder.hasClass('fa-chevron-right')) {
         iconHolder.removeClass('fa-chevron-right');
         iconHolder.addClass('fa-chevron-down');
-        if(element.parent().attr('id') == "chat-panel") {
-            chatPanelToggleUpCycle();
-        }
     }
-    else if(iconHolder.hasClass('fa-chevron-down')){
+    else if(iconHolder.hasClass('fa-chevron-down')) {
         iconHolder.removeClass('fa-chevron-down');
         iconHolder.addClass('fa-chevron-right');
-        if(element.parent().attr('id') == "chat-panel") {
-            chatPanelToggleDownCycle();
-        }
     }
+}
+
+/**
+ * chatToggle function
+ * @param chatHeader chatHeader bar
+ */
+function chatToggle(chatHeader) {
+    var iconHolderFirst = chatHeader.children().first();
+
+    if(iconHolderFirst.hasClass('fa-chevron-right')) {
+        chatHeader.next('.toggle_content').slideToggle();
+        toggleIcon(iconHolderFirst);
+        chatPanelToggleUpCycle();
+
+    }else if(iconHolderFirst.hasClass('fa-chevron-down')) {
+        // if we're minimizing it, we only want the new messages badge AFTER the transition is complete
+        chatHeader.next('.toggle_content').slideToggle(function(){
+            toggleIcon(iconHolderFirst);
+            chatPanelToggleDownCycle();
+        });
+    }
+
 }
